33b6d70f27bc6bde287dd08161000894c565515e
ladroff <ladroff@gmail.com>
2021-11-01 12:56:47 +0200

    Работа с классами , попытка разобраться )))

---
+++ Stuff.py
@@ -0,0 +1,12 @@
+class Stuff:
+
+    def __init__(self, shoes, wears, balls):
+        self.shoes = shoes
+        self.wears = wears
+        self.balls = balls
+
+    def get_order(self):
+        pass
+
+    def __str__(self):
+        return "Shoes = {}, Wears = {}, Balls = {}".format(self.shoes, self.wears, self.balls, )
